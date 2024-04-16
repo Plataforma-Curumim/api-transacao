6bd4244356e556d97841d1b20bd1213c1583c7b2
Cickruss <icaro.o.macedo@gmail.com>
2024-04-15 22:00:49 -0300

    Feat: Arquitetura Inicial funcional

---
+++ src/Adapters/Inbound/HTTP/VM/RequestUsuario.cs
@@ -0,0 +1,19 @@
+﻿namespace api_cadastro.Adapters.Inbound.HTTP.VM
+{
+    public record RequestUsuario
+    {
+
+        public string Nome { get; set; }
+        public string CpfCnpj { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Senha { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string IdRfid { get; set; }
+        public int Situacao { get; set; }
+        public int TipoUsuario { get; set; }
+
+    }
+}
---
+++ src/Domain/Core/Models/TransacaoUsuario.cs
@@ -0,0 +1,20 @@
+﻿using api_cadastro.Domain.Core.Enums;
+
+namespace api_cadastro.Domain.Core.Models
+{
+    public record TransacaoUsuario
+    {
+        public string Nome { get; set; }
+        public string CpfCnpj { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Senha { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string IdRfid { get; set; }
+        public EnumSituacaoUsuario Situacao { get; set; }
+        public EnumTiposUsuario TipoUsuario { get; set; }
+         
+    }
+}
